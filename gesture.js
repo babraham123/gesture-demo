06081009c22c097a0b76e6097db1f2867680aec4
Bereket Abraham <babraham@appnexus.com>
2015-10-12 15:37:55 -0400

    started web ui version

---
+++ gesture.js
@@ -0,0 +1,9 @@
+(function(){
+
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "green";
+    ctx.fillRect(10, 10, 100, 100);
+
+})();
